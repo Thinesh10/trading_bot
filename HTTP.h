9eecb11da8be6f0e504e3fa446b6ea240f609ca0
Thinesh <thinesh.m1996@gmail.com>
2021-11-21 18:21:39 +1100

    fix http.h requested changes

--- HTTP.h
+++ HTTP.h
@@ -1,36 +1,37 @@
 #include <curl/curl.h>
 #include <string>
-#include <map>
+#include <iostream>
+#include <string_view>
 #include <vector>
+#include <functional>
+#include <numeric>
 
 #pragma once
 
-static constexpr std::string_view the_curl_handler_err = "ERR: error creating curl handler";
-
-static size_t response_callback(void* aRespBuf, size_t aSize, size_t aNumMembers, void* aResJson)
-{
-    if(aResJson)
-    {
-        std::string& myResJson = *(static_cast<std::string*>(aResJson));
-        std::string::size_type myBufLen = aSize * aNumMembers;
-        myResJson.append(static_cast<char*>(aRespBuf), myBufLen);
-        return aNumMembers;
-    }
-
-    return 0;
-}
+static constexpr auto THE_CURL_HANDLER_ERR = "ERR: Could not create curl handler";
+static constexpr auto THE_HEADER_APPEND_ERR = "ERR: Could not append to header struct";
+static constexpr auto THE_USER_AGENT = "Trading Bot/0.1";
+static constexpr auto THE_APPLICATION_TYPE = "Content-Type: application/json";
 
 class HTTP {
 	private:
-		CURL *myHandle;
-		struct curl_slist *myHeaders; 
-		const std::string myUserAgent = "Trading Bot/0.1";
+		CURL *theHandle;
+		struct curl_slist *theHeaders = NULL; 
 
 	public:
-		HTTP () {
-			myHeaders = NULL;	
-		};
+		static size_t response_callback(void* , size_t, size_t, void*);
+		
+		HTTP()
+		{	
+			theHeaders = NULL;
+			theHeaders = curl_slist_append(theHeaders, THE_APPLICATION_TYPE);
 			
+			if (theHeaders == NULL) 
+			{
+				std::cout << THE_HEADER_APPEND_ERR << std::endl;
+			}
+		}
+				
 		CURLcode perform_request (
 			const std::string& aType,
 			const std::string& aURL,	
@@ -43,36 +44,64 @@ class HTTP {
 
 			curl_global_init(CURL_GLOBAL_ALL);
 			
-			myHandle = curl_easy_init();
+			theHandle = curl_easy_init();
 			
-			if (myHandle) {
-				curl_easy_setopt(myHandle, CURLOPT_CUSTOMREQUEST, aType.c_str());
-				curl_easy_setopt(myHandle, CURLOPT_URL, aURL.c_str());
-				curl_easy_setopt(myHandle, CURLOPT_USERAGENT, myUserAgent.c_str());
-				curl_easy_setopt(myHandle, CURLOPT_WRITEFUNCTION, &response_callback);
-    			curl_easy_setopt(myHandle, CURLOPT_WRITEDATA, aJsonRespBuf);
-				curl_easy_setopt(myHandle, CURLOPT_POSTFIELDS, aBody.c_str());	
+			if (theHandle) {
+				curl_easy_setopt(theHandle, CURLOPT_CUSTOMREQUEST, aType.c_str());
+				curl_easy_setopt(theHandle, CURLOPT_URL, aURL.c_str());
+				curl_easy_setopt(theHandle, CURLOPT_USERAGENT, THE_USER_AGENT);
+				curl_easy_setopt(theHandle, CURLOPT_WRITEFUNCTION, &response_callback);
+    			curl_easy_setopt(theHandle, CURLOPT_WRITEDATA, aJsonRespBuf);
+				curl_easy_setopt(theHandle, CURLOPT_POSTFIELDS, aBody.c_str());	
 
-				myHeaders = curl_slist_append(myHeaders, "Content-Type: application/json");
-				for (const auto& myHeaderEntry: aHeaders)
-					myHeaders = curl_slist_append(myHeaders, myHeaderEntry.c_str());
+				auto myHeaderAppendLamba = [](const auto& aHeaders, const std::string& aHeaderEntry)
+				{
+					if (aHeaders == NULL) 
+					{
+						return aHeaders;
+					}
+
+					return curl_slist_append(aHeaders, aHeaderEntry.c_str());					
+				};
+
+				theHeaders = std::accumulate(aHeaders.begin(), aHeaders.end(), theHeaders, myHeaderAppendLamba);
+				if (theHeaders == NULL) 
+				{
+					std::cout << THE_HEADER_APPEND_ERR << std::endl;
+					myResult = CURLE_FAILED_INIT;
+				} 
+				else 
+				{
+    				curl_easy_setopt(theHandle, CURLOPT_HTTPHEADER, theHeaders);
+					myResult = curl_easy_perform(theHandle);
+				}	
 			
-    			curl_easy_setopt(myHandle, CURLOPT_HTTPHEADER, myHeaders);
-
-				myResult = curl_easy_perform(myHandle);
-				curl_easy_cleanup(myHandle);
 			} 
 			else 
 			{
-				std::cout << the_curl_handler_err << std::endl;
-    			curl_global_cleanup();
-				return CURLE_FAILED_INIT;
+				std::cout << THE_CURL_HANDLER_ERR << std::endl;
+				myResult = CURLE_FAILED_INIT;
 			}
 			
+			curl_easy_cleanup(theHandle);
+			curl_slist_free_all(theHeaders);	
 			curl_global_cleanup();
+
 			return myResult;	
 		};
 
 		~HTTP() = default;	
 };
 
+size_t HTTP::response_callback(void* aRespBuf, size_t aSize, size_t aNumMembers, void* aResJson)
+{
+	if(aResJson)
+	{
+		std::string& myResJson = *(static_cast<std::string*>(aResJson));
+		std::string::size_type myBufLen = aSize * aNumMembers;
+		myResJson.append(static_cast<char*>(aRespBuf), myBufLen);
+		return aNumMembers;
+	}
+
+	return 0;
+}
